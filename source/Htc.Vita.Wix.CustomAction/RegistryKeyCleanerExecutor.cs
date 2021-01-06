25977b06945cdd94742bb0d06e6557a2117321d5
Kene Lin <kenelin@gmail.com>
2021-01-06 10:47:28 +0800

    Merge Deferred / Immediate subclasses into RegistryKeyCleanerExecutor

--- source/Htc.Vita.Wix.CustomAction/RegistryKeyCleanerExecutor.cs
+++ source/Htc.Vita.Wix.CustomAction/RegistryKeyCleanerExecutor.cs
@@ -5,8 +5,11 @@ using Microsoft.Win32;
 
 namespace Htc.Vita.Wix.CustomAction
 {
-    internal partial class RegistryKeyCleanerExecutor
+    internal class RegistryKeyCleanerExecutor
     {
+        private const string PrefixPath = "Path_";
+        private const string PrefixScope = "Scope_";
+
         internal class Deferred : AbstractActionExecutor
         {
             private const string ProfileListPath = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProfileList";
@@ -212,6 +215,43 @@ namespace Htc.Vita.Wix.CustomAction
                 }
                 Log("Can not convert Windows registry scope " + scope + ". Use LocalMachine as fallback scope");
                 return Scope.LocalMachine;
+            }
+        }
+
+        internal class Immediate : AbstractActionExecutor
+        {
+            public Immediate(Session session) : base("RegistryKeyCleanerExecutor.Immediate", session)
+            {
+            }
+
+            protected override ActionResult OnExecute()
+            {
+                var database = Session.Database;
+                if (!database.Tables.Contains("VitaRegistryKeyCleaner"))
+                {
+                    return ActionResult.Success;
+                }
+
+                try
+                {
+                    var view = database.OpenView("SELECT `Scope`, `Path` FROM `VitaRegistryKeyCleaner`");
+                    view.Execute();
+
+                    var customActionData = new CustomActionData();
+                    foreach (var row in view)
+                    {
+                        var index = Math.Abs(("" + row["Scope"] + "_" + row["Path"]).GetHashCode());
+                        customActionData[PrefixScope + index] = row["Scope"].ToString();
+                        customActionData[PrefixPath + index] = row["Path"].ToString();
+                    }
+
+                    Session["Vita_RegistryKeyCleanerDeferred"] = customActionData.ToString();
+                }
+                finally
+                {
+                    database.Close();
+                }
+                return ActionResult.Success;
             }
         }
 
